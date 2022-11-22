a8793009dc58fdd4e57032d4df9a9d15706e699a
kasahara <kasahara@lead-step.co.jp>
2022-11-22 14:23:37 +0900

    django drf tutoriral

--- ec-site-demo/vevn-ec-demo/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\kenzo\Desktop\サイト構築\django\github\python\ec-site-demo\vevn-ec-demo\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
