5d42bf8c18e63ababd2f5f96e8ab1cfc8fcf449f
kasahara <kasahara@lead-step.co.jp>
2022-11-22 14:21:43 +0900

    delete venv

---
+++ vevn-ec-demo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kenzo\Desktop\サイト構築\django\github\python\ec-site-demo\vevn-ec-demo\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
